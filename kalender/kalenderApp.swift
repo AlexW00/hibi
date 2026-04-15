20797404b41d2ca3cb6d333b3f165160bc1fd8a3
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2026-04-15 14:06:29 +0900

    Initial Commit

---
+++ kalender/kalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  kalenderApp.swift
+//  kalender
+//
+//  Created by Alex Weichart on 15.04.26.
+//
+
+import SwiftUI
+
+@main
+struct kalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
